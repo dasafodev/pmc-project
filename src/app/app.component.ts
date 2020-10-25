7593e07830e32824963970b0b22b23c63b9adb00
Felipe Delgado <af.delgador@uniandes.edu.co>
2020-10-24 19:09:54 -0500

    uodate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+declare var gtag;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pmc';
+
+  constructor(
+    private router: Router
+
+  ) {
+    const navEndEvents$ = this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      );
+
+    navEndEvents$.subscribe((event: NavigationEnd) => {
+      gtag('config', 'G-2RSTE0J2LD', {
+        'page_path': event.urlAfterRedirects
+      });
+    });
+  }
+
+
 }
